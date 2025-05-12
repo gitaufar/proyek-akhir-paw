30892b24d04093815503cc5b6353836a0352e962
gitaufar <zhafiraufar123@gmail.com>
2025-05-12 19:13:47 +0700

    auth beres

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
+
+Route::get('/list_modul', [MainController::class, 'showModul'])->name('list_modul');
+
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
+
+// Protected route
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
