21fae575a1201f53fa58662a9e48554eb5798e74
Anomaly03 <faridramdhani03@student.ub.ac.id>
2025-05-15 02:17:42 +0700

    habis rubah dashboard di rsource/views/dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,13 @@
-@extends('layout.app')
+@extends('layout.main')
 
 @section('content')
-<div class="p-6">Selamat datang di dashboard</div>
+<div class="relative w-full h-screen">
+    <!-- Lingkaran di kanan atas -->
+    <div class="absolute top-4 right-50 w-15 h-15 bg-red-100 rounded-full z-50"></div>
+    <div class="absoulute top-10 left-0 w-10 h-90 bg-amber-800 z-10"></div>
+
+</div>
+
+
+
 @endsection
