87687589141abf97e2b0a5343388a7c2ac9fbbdb
gitaufar <zhafiraufar123@gmail.com>
2025-05-14 13:59:26 +0700

    feats: add layout for community and modul

--- resources/views/list_modul.blade.php
+++ resources/views/list_modul.blade.php
@@ -1,11 +1,5 @@
-@extends('layout.app')
+@extends('layout.main')
 
 @section('content')
-
-<div class="min-w-screen min-h-screen bg-black items-center flex justify-center">
-
-<p class="text-amber-300 text-4xl font-bold">INI HALAMAN MODUL</p>
-
-</div>
 
 @endsection()
