f00efbe93367f5672a5b48fa1b876d0843f0ae1d
Anomaly03 <faridramdhani03@student.ub.ac.id>
2025-05-15 07:24:45 +0700

    modified resources/views.dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 <div class="relative w-full h-screen">
     <!-- Lingkaran di kanan atas -->
     <div class="absolute top-4 right-50 w-15 h-15 bg-red-100 rounded-full z-50"></div>
-    <div class="absoulute top-10 left-0 w-10 h-90 bg-amber-800 z-10"></div>
+    <div class="absoulute top-10 left-0 w-10 h-90 bg-amber-800 z-100"></div>
 
 </div>
 
